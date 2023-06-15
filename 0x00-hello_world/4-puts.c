e5217819ef05d0ee4ba27d10649118a5a6767ab5
Zaina <zainamge23@gmail.com>
2023-06-15 00:36:40 -0700

    using puts to print on the SO

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Description: print a string using puts function
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
