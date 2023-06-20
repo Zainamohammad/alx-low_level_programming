cdd5536c422519e1d5e13f014e18656df28e6d22
Zaina <zainamge23@gmail.com>
2023-06-20 06:05:36 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description - a program that displays the first
+ * 50 fibonacci numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	long long int sum, fir = 2, sec = 3;
+
+	for (i = 1; i < 4; ++i)
+	{	printf("%d", i);
+		if (i != 3)
+			printf(", ");
+	}
+	for (i = 4; i <= 50; ++i)
+	{
+		sum = fir + sec;
+		printf("%lld, ", sum);
+		if (i != 50)
+			printf(", ");
+		fir = sec;
+		sec = sum;
+	}
+	printf("\n");
+	return (0);
+}
+
