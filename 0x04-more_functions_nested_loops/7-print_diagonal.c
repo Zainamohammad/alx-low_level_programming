51620080d46a268ab00b4d4017ebad7575fbae02
Zaina <zainamge23@gmail.com>
2023-06-22 11:50:15 -0700

    function that prints backslash diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - prints diagonal using \
+ *
+ * @n: number of \ forming the diagonal
+ *
+ * Return - 0
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			if (i != 1)
+			{
+				for (j = 1; j <= (n - 1); j++)
+					_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
