735c8b9bb35f3da9e01228c16d4b1bf51468ccfa
Zaina <zainamge23@gmail.com>
2023-06-22 11:26:48 -0700

    edit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
  * print_line - a function that prints _ n times
- * @n - number of times _ is printed
+ *
+ * @n: number of times _ is printed
+ *
  * Return - 0
  */
 void print_line(int n)
@@ -9,12 +11,12 @@ void print_line(int n)
 	int i;
 
 	if (n > 0)
-	{	
-		for (i = 1; i <= n; ++i)	
-			_putchar('_');
-		_putchar('\n');
+	{
+		for (i = 1; i <= n; ++i)
+			_putchar(95);
+		/*_putchar('\n');*/
 	}
-	else 
-		_putchar('\n');
+	/*else*/
+	_putchar('\n');
 
 }
