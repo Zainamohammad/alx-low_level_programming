6257f478fd69428497f4a0bef14c4fa23db5b708
Zaina <zainamge23@gmail.com>
2023-06-22 10:51:54 -0700

    a function that prints _ n times)

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - a function that prints _ n times
+ * @n - number of times _ is printed
+ * Return - 0
+ */
+void print_line(int n)
+{
+	int n, i;
+
+	for (i = 1; i < n; i++)
+		_putchar(95);
+	_putchar('\n');
+}
