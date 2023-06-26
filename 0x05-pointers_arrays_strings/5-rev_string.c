5ee5fa572e9b3e4c203e84b28993e7b683c37899
Zainamohammad <129880162+Zainamohammad@users.noreply.github.com>
2023-06-27 02:27:54 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,19 +8,19 @@
  */
 void rev_string(char *s)
 {
-  	int i, count = 0;
+	int i, count = 0;
 	char str;
 
-	for (i = 0; *s != '\0'; ++i)
+	for (i = 0; s[i] != '\0'; ++i)
 	{
 		++count;
-		++s;
+		
 	}
-	--s;
+	
 	for (i = 0; i < count / 2; --i)
 	{
 		str = s[i];
 		s[i] = s[count - 1 - i];
 		s[count - 1 - i] = str;
-  }
+	}
 }
