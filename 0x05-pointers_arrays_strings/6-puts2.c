211dd4263abfd7f32ea2914a4403ef752acf488a
Zainamohammad <129880162+Zainamohammad@users.noreply.github.com>
2023-06-27 02:47:53 +0300

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * puts2 -  a function that prints every
+ * other character of a string
+ *
+ * @str: the string to be printed
+ *
+ * return: 0
+ */
+void puts2(char *str)
+{
