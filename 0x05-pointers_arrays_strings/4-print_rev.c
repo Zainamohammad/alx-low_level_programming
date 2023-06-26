d787f397a55d596710b0eb25e46fc47544f68e64
Zaina <zainamge23@gmail.com>
2023-06-26 09:24:05 -0700

    print reversed string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - a function that prints a string
+ * in reverse order
+ *
+ * @s: the string to be reversed
+ *
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int i, count = 0;
+
+	for (i = 0; *s != '\0'; ++i)
+	{
+		++count;
+		++s;
+	}
+	for (i = count; i >= 0; --i)
+	{
+		_putchar(*s);
+		--s;
+	}
+		
+	_putchar('\n');
+}
