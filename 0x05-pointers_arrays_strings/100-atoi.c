1560b2c8efa998c3dd57446dab65f65a42eea89d
Zainamohammad <129880162+Zainamohammad@users.noreply.github.com>
2023-06-27 22:17:12 +0300

    Create 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _atoi - a function that converts a string
+ * to an integer
+ *
+ * @s: the string to be converted
+ *
+ * return: 0
+ */
+int _atoi(char *s)
+{
+	int i, count = 0;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		++count;
+    if (sizeof(s[i]) < 4)
+    {
+      s[i + 1] = s[i];
+      --count;
+    }
+  }
+  if (count = 0)
+    return (0);
+  else
+    return(s);
+}
