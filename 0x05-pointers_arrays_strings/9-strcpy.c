566b83137f8fd1cf0f422c558c91fa8ccb772582
Zainamohammad <129880162+Zainamohammad@users.noreply.github.com>
2023-06-27 04:37:31 +0300

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcpy - a function that copies a string
+ * into a buffer
+ *
+ * @dest: a pointer to the buffer
+ * @src: the string to be copied
+ *
+ * return: a pointer to the buffer (dest)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, count = 0;
+
+	for (i = 0; src[i] != '\0'; ++i)
+	{
+		++count;
+	}
+for (i = 0; i < count; ++i)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
