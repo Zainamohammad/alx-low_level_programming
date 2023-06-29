3a47dae40edcfccf571d697c41d88f093c8e2209
Zaina <zainamge23@gmail.com>
2023-06-29 08:55:51 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j, count = 0, length = 0, total = 0;
+	int i, j, count = 0, length = 0/*, total = 0*/;
 
 	for (i = 0; dest[i] != '\0'; ++i)
 		++count;
 	for (j = 0; src[j] != '\0'; ++j)
 		++length;
-	total = count + length;
-	for (i = count, j = 0; i <= total; ++i, ++j)
+	/*total = count + length;*/
+	for (i = count, j = 0; j < length; ++i, ++j)
 	{
 		dest[i] = src[j];
 	}
