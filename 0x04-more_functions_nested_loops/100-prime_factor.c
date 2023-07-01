e4c18c5eef1c5b65337e26369a53431af78de3e3
Zainamohammad <129880162+Zainamohammad@users.noreply.github.com>
2023-07-02 02:51:30 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define MAX 100000000000
 /**
  * main - entry point
  *
@@ -9,4 +10,22 @@
  */
 int main(void)
 {
+  int i, arr[MAX], j, k = 0;
+
+  for (i = 2; i < 612852475143; ++i)
+{
+for (j = 2; j < 612852475143; ++j)
+{
+if (i > j)
+{
+if (i % j != 0)
+{
+arr[k] = i;
+++k;
+}
+else
+  countinue;
+}
+}
+}
 
