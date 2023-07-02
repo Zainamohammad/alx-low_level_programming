21f6b17e93909475d62d8bcd34e0e13229f57942
Zainamohammad <129880162+Zainamohammad@users.noreply.github.com>
2023-07-02 04:15:43 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
+
 #define MAX 100000000000
+
 /**
- * main - entry point
+ * main - Entry point
  *
- * Description - a program that finds the highest
- * prime factor of a number
+ * Description: A program that finds the highest prime factor of a number
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  unsigned long int i, arr[MAX], j, k = 0;
+	unsigned long int i, arr[MAX], j, k = 0;
 
-  for (i = 2; i < 612852475143; ++i)
-{
-for (j = 2; j < 612852475143; ++j)
-{
-if (i > j)
-{
-if (i % j != 0)
-{
-arr[k] = i;
-++k;
-}
-else
-  continue;
-}
-}
-}
-  for (i = 0; i <= k; ++i)
-    {
-      while (612852475143 % arr[i] == 0)
-        printf("%lu\n", arr[i]);
-    }
-  return (0);
-}
+	for (i = 2; i < 612852475143; ++i)
+	{
+		for (j = 2; j < 612852475143; ++j)
+		{
+			if (i > j)
+			{
+				if (i % j != 0)
+				{
+					arr[k] = i;
+					++k;
+				}
+				else
+					continue;
+			}
+		}
+	}
 
+	for (i = 0; i <= k; ++i)
+	{
+		while (612852475143 % arr[i] == 0)
+			printf("%lu\n", arr[i]);
+	}
+
+	return (0);
+}
