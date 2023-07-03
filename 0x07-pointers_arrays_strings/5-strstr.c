cf35842ff68ca0e03c122eae219f1f7a6e539260
Zaina <zainamge23@gmail.com>
2023-07-03 16:42:06 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,18 +10,16 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, check = 0;
+	int i, j;
 
 	for (i = 0; haystack[i] != '\0'; ++i)
 	{
 		if (haystack[i] == *needle)
 		{
-			check = 1;
 			for (j = 0; needle[j] != '\0'; ++j)
 			{
 				if (haystack[i + j] != needle[j])
 				{
-					check = 0;
 					break;
 				}
 			}
