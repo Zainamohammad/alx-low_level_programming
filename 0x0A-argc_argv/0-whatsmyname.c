35be4d069b1c10d2951caf783a936071051cb042
Zaina <zainamge23@gmail.com>
2023-07-07 08:24:30 -0700

    my name is

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - entry point
+ *
+ * Description: print the name of the program
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	(void) argc;
