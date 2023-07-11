8cf832cde99952f387457f07af665dc056bacf12
Zaina <zainamge23@gmail.com>
2023-07-11 06:13:34 -0700

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,26 +17,24 @@ char *str_concat(char *s1, char *s2)
 	int i, j, ls1 = 0, ls2 = 0;
 	char *new;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; ++i)
 		++ls1;
 	for (i = 0; s2[i] != '\0'; ++i)
 		++ls2;
-	if (s1 == NULL)
-		ls1 = 0;
-	if (s2 == NULL )
-		ls1 = 0;
 	new = malloc( 1 + sizeof(char) * (ls1 + ls2));
 	if (new == NULL)
 		return ('\0');
 	for (i = 0; s1[i] != '\0'; ++i)
 	{
-		if (ls1 != 0)
-			new[i] = s1[i];
+		new[i] = s1[i];
 	}
 	for (i = ls1, j = 0; s2[j] != '\0'; ++i, ++j)
 	{
-		if (ls2 != 0)
-			new[i] = s2[j];
+		new[i] = s2[j];
 	}
 	return (new);
 }
