b0ee372fb72827aaad6893f0f6dce34cc9d1e993
Zaina <zainamge23@gmail.com>
2023-07-13 14:02:53 -0700

    string concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	str = malloc(sizeof(char) * (len1 + len2) + 1);
 	if (str == NULL)
 		return ('\0');
-	for (i = 0; i < len1; ++i)
+	for (i = 0; s1[i] != '\0'; ++i)
 		str[i] = s1[i];
 	for (i = len1; i < len1 + n; ++i, ++j)
 		str[i] = s2[j];
