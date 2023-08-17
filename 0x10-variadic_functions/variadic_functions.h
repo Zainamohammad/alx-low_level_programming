3dcd9fe3771b1864957329519abe1ebdbf296611
Zainamohammad <zainamge23@gmail.com>
2023-08-17 12:40:42 -0700

    edit betty

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,22 @@ int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+/**
+ * struct format - a new type
+ *
+ * @x: the format specifier
+ * @f: a function pointer to print a data type according
+ * to the format specifier
+ */
+typedef struct format
+{
+	char *x;
+	void (*f)(va_list args);
+} format_spec;
+void print_all(const char * const format, ...);
+void print_string(va_list args);
+void print_float(va_list args);
+void print_int(va_list args);
+void print_char(va_list args);
 #endif
 
