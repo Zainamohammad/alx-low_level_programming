71be20343ab07b201004c92b6994ff28cc605233
Zainamohammad <zainamge23@gmail.com>
2023-09-07 07:48:55 -0700

    delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,6 +13,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *pn, *px;
 
+	if (*head == NULL)
+		return (-1);
 	if (index == 0)
 	{
 		pn = *head;
