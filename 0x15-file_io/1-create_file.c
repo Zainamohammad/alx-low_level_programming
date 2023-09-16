3b521730f31da4be404b3946f1d91d21ddf430db
Zainamohammad <zainamge23@gmail.com>
2023-09-16 09:59:28 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,14 +15,17 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC, 0600);
+	fd = creat(filename, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-		text_content = "";
-	len = strlen(text_content);
-	write_check = write(fd, text_content, len);
-	if (write_check == -1)
-		return (-1);
+		return (1);
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		write_check = write(fd, text_content, len);
+		if (write_check == -1)
+			return (-1);
+	}
 	return (1);
 }
